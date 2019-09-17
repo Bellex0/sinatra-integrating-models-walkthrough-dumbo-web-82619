f4bb9e53be8da6f5bc7e08825d71f9f548c811e4
Belle P <bellepx3@gmail.com>
2019-09-17 14:41:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,9 @@ class App < Sinatra::Base
 
   post '/' do
      @analyzed_text = TextAnalyzer.new(params[:user_text])
-     "#{@analyzed_text}"
+     
 
     erb :results
+    "#{@analyzed_text}"
   end
 end
